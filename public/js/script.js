37bc77da33a1d10e106b01635e9cb60cd5e82acf
AkhilaKolluru98 <akhilakolluru020@gmail.com>
2022-05-03 21:32:51 +1000

    -Commits - Added home page

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+//SideNav
+ const sideNav= document.querySelector('.sidenav');
+ M.Sidenav.init(sideNav, {});
+
+ //Slider
+ const slider=document.querySelector('.slider');
+ M.Slider.init(slider, {
+     indicators:false,
+     height:500,
+     transition: 500,
+     interval: 6000
+ });
