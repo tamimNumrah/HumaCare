b190860be54740ff2c10d5c94d435c5cc6fadd90
tamimNumrah <tamim@numrah.com>
2022-05-14 18:59:39 +1000

    Fixing patients login

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const expressEjsLayout = require("express-ejs-layouts");
 const session = require("express-session");
 const flash = require("connect-flash"); //for flash message
 const passport = require('passport');
-require("./config/passport")(passport)
+require("./config/patientLocalStrategy")(passport)
 
 let dbConnect = require("./dbConnect");
 
